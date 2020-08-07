b94a62d920d5cfa8d4151ae6c47e41b998b4a0f0
Martin <martinyanez420@gmail.com>
2020-08-07 10:35:53 -0300

    cover.html changes

--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
+var categoriesArray = [];
+
+function showCategoriesList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                        <h4>` + category.name + `</h4>
+                        <p>` + category.description + `</p>
+                        </div>
+                        <small class="text-muted">` + category.productCount + ` artículos</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
