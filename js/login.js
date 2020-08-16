430c427c63d5fce8ee5c92cc53bb17bb123834f3
ysuran92 <martinyanez420@gmail.com>
2020-08-16 09:24:54 -0300

    google login button changed

--- js/login.js
+++ js/login.js
@@ -1,6 +1,41 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var googleUser = {};
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+
+function onFailure(error) {
+    console.log(error);
+}
+
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
+
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+            document.getElementById('name').innerText = "Signed in: " +
+                googleUser.getBasicProfile().getName();
+        },
+        function(error) {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
+
 document.addEventListener("DOMContentLoaded", function(e) {
 
 });
