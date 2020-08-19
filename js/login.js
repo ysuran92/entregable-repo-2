30fbedeb5417dc1862d0362c441ae0481d41012f
ysuran92 <martinyanez420@gmail.com>
2020-08-19 08:06:17 -0300

    test

--- js/login.js
+++ js/login.js
@@ -1,6 +1,3 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 var googleUser = {};
 
 function onSuccess(googleUser) {
@@ -53,7 +50,9 @@ function attachSignin(element) {
     }, false);
 })();
 
-//
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
 
 });
