41268831f75c9f3ae4944a5f8b059783a3d99b82
ysuran92 <martinyanez420@gmail.com>
2020-08-22 15:33:29 -0300

    various login fixes

--- js/login.js
+++ js/login.js
@@ -54,6 +54,26 @@ function attachSignin(element) {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
+    document.getElementById("submitBtn").addEventListener("click", function(e) {
+        let inputEmail = document.getElementById("inputEmail");
+        let validationCustomUsername = document.getElementById("validationCustomUsername");
+        let camposCompletos = true;
 
+        if (inputEmail.value === '') {
+            inputEmail.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (validationCustomUsername.value === '') {
+            validationCustomUsername.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (camposCompletos) {
+
+
+            localStorage.setItem("User-Logged", JSON.stringify({ email: inputEmail.value }));
+
+        }
+    })
 });
-//
