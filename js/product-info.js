5e2d019861a435e53766a1c6b760570a0caf315f
ysuran92 <martinyanez420@gmail.com>
2020-09-06 16:21:29 -0300

    products show info

--- js/product-info.js
+++ js/product-info.js
@@ -1,57 +1,73 @@
-let productsArray = [];
-let comentariosArray = [];
+var product = {};
 
-function showProduct(array, arrayComments) {
-    let verProducto = JSON.parse(localStorage.getItem("product"));
-    for (let i = 0; i < productsArray.length; i++) {
-        let product = array[i];
-        let info = "";
-        let imgs = "";
-        let comments = "<hr>";
+function showImagesGallery(array) {
 
-        if (product.name == verProducto.productid) {
-            info += "<h2>" + product.name + "</h2>";
-            info += "<strong>" + product.cost + "</strong><br><br>";
-            info += "" + product.description + "<br>";
-            info += libro.isbn + "<br>";
-            info += libro.paginas + " págs.<br>";
+    let htmlContentToAppend = "";
 
-            imgs += '<img class="img" src="img/' + libro.titulo +
-                '/1.jfif" width="100px" height="150px" alt="">';
-            imgs += '<img class="img" src="img/' + libro.titulo +
-                '/2.jfif" width="100px" height="150px" alt="">';
-            imgs += '<img class="img" src="img/' + libro.titulo +
-                '/3.jfif" width="100px" height="150px" alt="">';
+    for (let i = 0; i < array.length; i++) {
+        let imageSrc = array[i];
 
-            for (let comment in arrayComments) {
-                if (arrayComments[comment].id_libro == verLibro.productid) {
-                    comments += "<strong>" + arrayComments[comment].usuario + "</strong> dice: <br>";
-                    comments += "<p>" + arrayComments[comment].comentario + "</p><br>";
-                    comments += "Calificación: <strong>" + arrayComments[comment].calificacion + "</strong><br>";
-                    comments += "<br><hr>"
-                }
-            }
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
 
-            document.getElementById("contenido").innerHTML = info;
-            document.getElementById("imagenes").innerHTML = imgs;
-            document.getElementById("comentarios").innerHTML = comments;
-        }
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
     }
-
-
 }
 
+function showComments(arrayComments) {
+    let verLibro = JSON.parse(localStorage.getItem("libro"));
+    for (let i = 0; i < librosArray.length; i++) {
+        let comments = "<hr>";
+
+
+
+        for (let comment in arrayComments) {
+            if (arrayComments[comment].id_libro == verLibro.libroid) {
+                comments += "<strong>" + arrayComments[comment].usuario + "</strong> dice: <br>";
+                comments += "<p>" + arrayComments[comment].comentario + "</p><br>";
+                comments += "Calificación: <strong>" + arrayComments[comment].calificacion + "</strong><br>";
+                comments += "<br><hr>"
+            }
+        }
+
+
+        document.getElementById("comentarios").innerHTML = comments;
+    }
+}
+
+
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let soldCountHTML = document.getElementById("soldCount");
+            let productCategoryHTML = document.getElementById("productCriteria");
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            soldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+    });
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             comentariosArray = resultObj.data;
-        }
-    });
-
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
-        if (resultObj.status === "ok") {
-            productsArray = resultObj.data;
-            showProduct(productsArray, comentariosArray);
         }
     });
 });
