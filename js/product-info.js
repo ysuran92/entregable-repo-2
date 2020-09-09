db01cfe3f096279b756d23368cd965135bf0367b
ysuran92 <martinyanez420@gmail.com>
2020-09-09 10:02:05 -0300

    send comment+rating WIP

--- js/product-info.js
+++ js/product-info.js
@@ -34,16 +34,51 @@ function showComments(arrayComments) {
         for (let i = comment.score; i < 5; i++) {
             score += ` <span class="fa fa-star"></span> `;
         }
-
         comments += ` <sub> ${comment.dateTime} </sub> <br> `
-
         comments += ` <div style="text-align: right;"> ${score} </div> `;
     })
     let commentsHTML = document.getElementById("comentarios");
     commentsHTML.innerHTML = comments;
 }
 
+//////////////////// STAR RATING START
 
+jQuery(document).ready(function($) {
+    $('.rating_stars span.r').hover(function() {
+        // get hovered value
+        var rating = $(this).data('rating');
+        var value = $(this).data('value');
+        $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_' + rating);
+        highlight_star(value);
+    }, function() {
+        // get hidden field value
+        var rating = $("#rating").val();
+        var value = $("#rating_val").val();
+        $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_' + rating);
+        highlight_star(value);
+    }).click(function() {
+        // Set hidden field value
+        var value = $(this).data('value');
+        $("#rating_val").val(value);
+
+        var rating = $(this).data('rating');
+        $("#rating").val(rating);
+
+        highlight_star(value);
+    });
+
+    var highlight_star = function(rating) {
+        $('.rating_stars span.s').each(function() {
+            var low = $(this).data('low');
+            var high = $(this).data('high');
+            $(this).removeClass('active-high').removeClass('active-low');
+            if (rating >= high) $(this).addClass('active-high');
+            else if (rating == low) $(this).addClass('active-low');
+        });
+    }
+});
+
+///////////////////// STAR RATING END
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
